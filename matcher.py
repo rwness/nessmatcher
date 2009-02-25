7ecfe713f54dc53d567c34b8decbccd28c76ff08
Justin Giancola <justin.giancola@gmail.com>
2009-02-24 21:59:49 -0500

    using 'sliding window' patterns

--- matcher.py
+++ matcher.py
@@ -1,13 +1,13 @@
 import re
 
-class MatcherNess:
+class Matcher:
 	def __init__(self, adapter):
 		self.adapter = adapter
 		self.rc = reverse_complement(self.adapter)
 		self.forward_seeker = re.compile('(?P<preseq>.*?)' + make_search_pattern(self.adapter) + '(?P<postseq>.*)')
 		self.rc_seeker = re.compile ('(?P<preseq>.*?)' + make_search_pattern(self.rc) + '(?P<postseq>.*?)')
 
-	def match_forward(self, sequence, ext_threshold = 5, int_threshold = 10):
+	def match_forward(self, sequence, ext_threshold = 5, int_threshold = 15):
 		match_forward = self.forward_seeker.search(sequence)
 		if match_forward and len(match_forward.group('adapterseq')) > int_threshold:
 			return match_forward.group('postseq')
@@ -16,7 +16,7 @@ class MatcherNess:
 		else:
 			return None
 
-	def match_rc(self, sequence, ext_threshold = 5, int_threshold = 10):
+	def match_rc(self, sequence, ext_threshold = 5, int_threshold = 15):
 		match_rc = self.rc_seeker.search(sequence)
 		if match_rc and len(match_rc.group('adapterseq')) > int_threshold:
 			return match_rc.group('preseq')
@@ -25,22 +25,27 @@ class MatcherNess:
 		else:
 			return None
 
-	def match(self, sequence, ext_threshold = 5, int_threshold = 10):
+	def match(self, sequence, ext_threshold = 5, int_threshold = 15):
 		self.match_forward(sequence, ext_threshold, int_threshold)
 		return self.match_forward(sequence, ext_threshold, int_threshold) or self.match_rc(sequence, ext_threshold, int_threshold)
 
 # helper functions
-
 def make_search_pattern(adapter):
-	alternates = []
 	threshold = 5
-	alternates.append(adapter)
-	i = len(adapter) -1
-	while i >= threshold:
-		alternates.append(adapter[:i])
-		alternates.append(adapter[-i:])
-		i -= 1
-	return "(?P<adapterseq>" + "|".join(alternates) + ")"
+	windows = []
+	window_size = len(adapter)
+	i = 0
+	j = (i + window_size-1)
+
+	while window_size >= threshold:
+		i = 0
+		j = j = (i + window_size)
+		while j <= len(adapter):
+			windows.append(adapter[i:j])
+			i += 1
+			j = (i + window_size)
+		window_size -= 1
+	return "(?P<adapterseq>" + "|".join(windows) + ")"
 
 # this function takes the adapter and generates the reverse complement
 def reverse_complement(adapter):
--- test_matcher.py
+++ test_matcher.py
@@ -1,32 +1,34 @@
-import matcher_ness
-from matcher_ness import MatcherNess
+import matcher
+from matcher import Matcher
 import unittest
 
 class TestMatcher(unittest.TestCase):
-    def setUp(self):
-        self.m = MatcherNess('AAGCAGTGGTATCAACGCAGAGTACGCGGG')
+	def setUp(self):
+		self.m = Matcher('AAGCAGTGGTATCAACGCAGAGTACGCGGG')
 
-    def test1(self):
-        self.assertEquals('CAACT',(self.m.match('CAACTCCCGCGTACTCTGCGTTGATACCACTGCTTACTCT')))
+	def test1(self):
+		self.assertEquals('CAACT',(self.m.match('CAACTCCCGCGTACTCTGCGTTGATACCACTGCTTACTCT')))
 
-    def test2(self):
-        self.assertEquals('GTTGATACCGCTGCT',(self.m.match('GTTGATACCGCTGCTTACTCTGCGTTGATACCACTGCTTA')))
+	def test2(self):
+		self.assertEquals('' ,(self.m.match('GTTGATACCGCTGCTTACTCTGCGTTGATACCACTGCTT')))
 
-    def test3(self):
-        self.assertEquals('GACGACTGATCGATC',(self.m.match('GACGACTGATCGATCTACTCTGCGTTGATACCACG')))
+	def test3(self):
+		self.assertEquals('GACGACTGATCGATC',(self.m.match('GACGACTGATCGATCTACTCTGCGTTGATACCACG')))
 
-    def test4(self):
-        self.assertEquals('GCTACTGATCATATCGTTAGCTAGCTAGCTACGT',(self.m.match('GCTACTGATCATATCGTTAGCTAGCTAGCTACGTCGTACT')))
+	def test4(self):
+		self.assertEquals('GCTACTGATCATATCGTTAGCTAGCTAGCTACGT',(self.m.match('GCTACTGATCATATCGTTAGCTAGCTAGCTACGTCGTACT')))
 
-    def test5(self):
-        self.assertEquals('GAGCCAGGCT',(self.m.match('GCAGTGGTATCAACGCAGAGTACGCGGGGAGCCAGGCT')))
+	def test5(self):
+		self.assertEquals('GAGCCAGGCT',(self.m.match('GCAGTGGTATCAACGCAGAGTACGCGGGGAGCCAGGCT')))
 
-    def test6(self):
-        self.assertEquals('AAGCAGTGGTATCAACGCAGA',(self.m.match('CAACGCAGAGTACGCGGGAAGCAGTGGTATCAACGCAGA')))
+	def test6(self):
+		self.assertEquals('AAGCAGTGGTATCAACGCAGA',(self.m.match('CAACGCAGAGTACGCGGGAAGCAGTGGTATCAACGCAGA')))
 
+	def test7(self):
+		self.assertEquals('TCT',(self.m.match('AGCTAGCTAGCTAGCTAATCAACGCAGAGTACGCGGGTCT')))
 
-    def test7(self):
-        self.assertEquals(None,(self.m.match('GCAGTGGTATCAACGCAGAGTAAGCCGTGGTATCAACGCA')))
+	def test8(self):
+		self.assertEquals('AGCCGTGGTATCAACGCA',(self.m.match('GCAGTGGTATCAACGCAGAGTAAGCCGTGGTATCAACGCA')))
 
 if __name__ == "__main__":
     unittest.main()
