3a9ecdfc6afbce66b220fecec691773574a16adc
Justin Giancola <justin.giancola@gmail.com>
2009-02-25 00:47:05 -0500

    list comprehensions FTW!

--- matcher.py
+++ matcher.py
@@ -6,8 +6,8 @@ class Matcher:
 
 	def __init__(self, adapter):
 		self.rc = reverse_complement(adapter)
-		self.pattern = re.compile('(?P<auxseq>.*?)' + make_search_pattern(adapter) + '(?P<targetseq>.*)')
-		self.rc_pattern = re.compile ('(?P<targetseq>.*?)' + make_search_pattern(self.rc) + '(?P<auxseq>.*?)')
+		self.pattern = re.compile('(?P<auxseq>.*?)' + make_pattern(adapter) + '(?P<targetseq>.*)')
+		self.rc_pattern = re.compile ('(?P<targetseq>.*?)' + make_pattern(self.rc) + '(?P<auxseq>.*?)')
 
 	def match_pattern(self, sequence, pattern):
 		match = pattern.search(sequence)
@@ -20,21 +20,9 @@ class Matcher:
 		return self.match_pattern(sequence, self.pattern) or self.match_pattern(sequence, self.rc_pattern)
 
 # helper functions
-def make_search_pattern(adapter):
-	windows = []
-	window_size = len(adapter)
-	i = 0
-	j = (i + window_size-1)
-
-	while window_size >= Matcher.EXT_THRESHOLD:
-		i = 0
-		j = j = (i + window_size)
-		while j <= len(adapter):
-			windows.append(adapter[i:j])
-			i += 1
-			j = (i + window_size)
-		window_size -= 1
-	return "(?P<adapterseq>" + "|".join(windows) + ")"
+def make_pattern(adapter):
+	segments = [adapter[j:i + j] for i in range(5,len(adapter) + 1)[::-1] for j in range(0,len(adapter)) if i + j <= len(adapter)]
+	return "(?P<adapterseq>" + "|".join(segments) + ")"
 
 def reverse_complement(adapter):
 	compliments = {'A':'T', 'C':'G', 'G':'C', 'T':'A'}
